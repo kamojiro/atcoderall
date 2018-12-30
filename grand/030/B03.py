f85cfd8ffdf25ad717565180eaa91b8636fbf8c0
kamojiro <tamagoma002@yahoo.co.jp>
2018-12-30 22:05:09 +0900

    整理

---
+++ grand/030/B03.py
@@ -0,0 +1,13 @@
+L, N = map( int, input().split())
+X = [ int( input()) for _ in range(N)]
+L = [0]*(N+1)
+R = [0]*(N+1)
+RX = [ L - X[i] for i in range(N-1,-1,-1)]
+for i in range(N):
+    L[i+1] = L[i]*2 + X[i]
+    R[i+1] = R[i]*2 + X[i]
+
+#初めの位置から右左に順番に往復する場合
+ans = L[N//2] + R[(N+1)//2]
+print(ans)
+    
