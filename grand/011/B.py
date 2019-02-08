39a13df196fd196310f832fc1e444d6f5e6a059c
kamojiro <tamagoma002@yahoo.co.jp>
2019-02-09 03:52:24 +0900

    少し早くできることに気づいた。100msくらい改善した。

--- grand/011/B.py
+++ grand/011/B.py
@@ -1,15 +1,19 @@
+from itertools import accumulate
 N = int( input())
 A = list( map( int, input().split()))
 A.sort()
 l = -1
 r = N-1
+B = list( accumulate(A))
 while r - l > 1:
     m = (l+r)//2
-    now = sum(A[:m+1])
     check = 1
+    #now = sum(A[:m+1])
     for i in range(m+1,N):
-        if A[i] <= now*2:
-            now += A[i]
+        if A[i] <= B[i-1]*2:
+            pass
+        # if A[i] <= now*2:
+        #     now += A[i]
         else:
             check = 0
             break
