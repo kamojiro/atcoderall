1a326cbb4bf1c8fd9d23cfb02ff43604c1ebc616
kamojiro <tamagoma002@yahoo.co.jp>
2019-05-14 20:56:38 +0900

    はい

--- company/CPSCO/2019/session3/D2.py
+++ company/CPSCO/2019/session3/D2.py
@@ -3,10 +3,10 @@ S = input()
 ans = "Yes"
 T = [0]*N
 T[0] = 1
-if S[0] == "R":
+if S[0] != "R":
     ans = "No"
 else:
-    for i in range(2,N):
+    for i in range(1,N):
         if T[i-1] == 1:
             if S[i] == "R":
                 T[i] = 1
@@ -15,23 +15,22 @@ else:
             else:
                 ans = "No"
                 break
-        if T[i-1] == 2:
+        elif T[i-1] == 2:
             if S[i] == "R":
                 T[i] = 1
             elif S[i] == "B":
-                T[i] = 2
+                T[i] = 3
             else:
                 ans = "No"
                 break
-        if T[i-1] == 3:
+        elif T[i-1] == 3:
             if S[i] == "R":
                 T[i] = 1
             elif S[i] == "G":
                 T[i] = 4
             else:
-                ans = "No"
-                break
-        if T[i-1] == 4:
+                T[i] = 3
+        elif T[i-1] == 4:
             if S[i] == "R":
                 T[i] = 1
             elif S[i] == "B":
@@ -39,12 +38,14 @@ else:
             else:
                 ans = "No"
                 break
-        if T[i-1] == 5:
+        elif T[i-1] == 5:
             if S[i] == "R":
                 T[i] = 1
-            elif S[i] == "B":
-                T[i] = 5
+            elif S[i] == "G":
+                T[i] = 4
             else:
-                ans = "No"
-                break
+                T[i] = 5
 
+if T[-1] == 1 or T[-1] == 2 or T[-1] == 4:
+    ans = "No"
+print(ans)
