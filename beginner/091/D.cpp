2140ea0eff61501fdf7a8d5ffce1c141dfdf82b2
kamojiro <tamagoma002@yahoo.co.jp>
2019-07-21 16:02:45 +0900

    ABC091D,TwoSequenceを通した。C++じゃないと無理。

--- beginner/091/D.cpp
+++ beginner/091/D.cpp
@@ -4,36 +4,45 @@
 #include <string>
 #include <algorithm>
 #include <climits>
+#include <math.h>
 using namespace std;
 typedef long long int ll;
 #define all(x) x.begin(),x.end()
 int main()
 {
-  int N, a, now, t;
+  int N;
+  ll a, t, now;
+  int p;
   cin >> N;
-  vector<int> vec;
+  vector<int> vecA;
+  vector<int> vecB;
   for (int i = 0; i < N; ++i) {
     cin >> a;
-    vec.push_back(a);
+    vecA.push_back(a);
+  }
+  for (int i = 0; i < N; ++i) {
+    cin >> a;
+    vecB.push_back(a);
   }
   vector<int> check(N);
-  int[] ans = int[30];
+  int ans[30];
   for (int i = 0; i < 30; ++i) {
     now = 0;
-    t = 2**i;
+    t = pow(2,i);
     for (int j = 0; j < N; ++j) {
-      check[j] = vec[j]%(2*t);
-    }
-    sort(check.all);
-    for (int j = 0; j < N; ++j) {
-      check[j] = A[j]%(t*2);
-      now += (lower_bound(all(check),t*2-a) - bisect_left(D, t-a)) + (bisect_left(D, 4*t-a) - bisect_left(D, t*3-a))
+      check[j] = vecA[j]%(2*t);
     }
     sort(all(check));
-    
-         
-
+    for (int j = 0; j < N; ++j) {
+      a = vecB[j]%(t*2);
+      now += (lower_bound(all(check),t*2-a) - lower_bound(all(check), t-a)) + (lower_bound(all(check), 4*t-a) - lower_bound(all(check), t*3-a));
+    }
+    ans[i] = now%2;
   }
-
+  ll ret = 0;
+  for (int i = 0; i < 30; ++i) {
+    ret += ans[i]*pow(2,i);
+  }
+  cout << ret << "\n";
   return 0;
 }
