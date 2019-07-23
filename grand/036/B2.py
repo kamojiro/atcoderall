62c14f377a9cb644665cc5df80ed836bab4f9daa
kamojiro <tamagoma002@yahoo.co.jp>
2019-07-23 20:57:38 +0900

    考え中

--- grand/036/B2.py
+++ grand/036/B2.py
@@ -1,28 +1,31 @@
 #import sys
 #input = sys.stdin.readline
 from collections import defaultdict, deque
+from math import log2
 def main():
     n, k = map( int, input().split())
     A = list( map( int, input().split()))
-    q = deque()
-    d = defaultdict( int)
-    l = 0
-    for k in range(k):
-        for a in A:
-            if d[a] == 0:
-                q.append(a)
-                d[a] = 1
-                continue
-            d[a] = 0
-            while q:
-                t = q.pop()
-                d[t] = 0
-                if t == a:
-                    break
-        if !q:
-            l = k+1
-            break
-    
-
+    logNK = int(log2(n) + log2(k))
+    double = [ [0]*(logNK+1) for _ in range(n)]
+    d = defaultdict( lambda :-1)
+    for i in range(n):
+        if d[A[i]] == -1:
+            d[A[i]] = i
+            continue
+        double[d[A[i]]][0] = (i+1)%N
+        d[A[i]] = i
+    for i in range(n):
+        if d[A[i]] > -1:
+            double[d[A[i]]][0] = (i+1)%N
+            d[A[i]] = -1
+    for i in range(logNK):
+        for j in range(N):
+            double[j][i+1] = double[double[j][i]][i]
+    L = N*K
+    now = 0
+    for i in range(logNK, -1, -1):
+        if pow(2,i) < 
+        L -= pow(2, i)
+        now
 if __name__ == '__main__':
     main()
