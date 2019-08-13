d00a6aaf9f6d2a84c5f0527afef375b4a70cfff8
kamojiro <tamagoma002@yahoo.co.jp>
2019-08-13 23:36:14 +0900

    EのBellman-Fordを通した。多重辺を考慮せずに辺のコストの情報をdictで持ってたのが原因だった。

--- beginner/137/E.py
+++ beginner/137/E.py
@@ -9,25 +9,60 @@ def main():
         a, b = a-1, b-1
         d[(a,b)] = P-c
 
-    INF = float("inf")
-    V = [INF]*N
+    V = [None]*N
     V[0] = 0
+    for _ in range(N-1):
+        for e in d:
+            s, t = e
+            if V[s] == None:
+                continue
+            if V[t] == None:
+                V[t] = V[s] + d[e]
+                continue
+            if V[s] + d[e] < V[t]:
+                V[t] = V[s] + d[e]
+    W = [V[i] for i in range(N)]
+    for e in d:
+        s, t = e
+        if V[s] == None:
+            continue
+        if V[t] == None:
+            V[t] = V[s] + d[e]
+            continue
+        if V[s] + d[e] < V[t]:
+            V[t] = V[s] + d[e]
+    for i in range(N):
+        if V[i] == None or W[i] == None:
+            continue
+        if V[i] < W[i]:
+            V[i] = "a"
     for _ in range(N):
         for e in d:
             s, t = e
+            if V[s] == "a" or V[t] == "a":
+                V[t] == "a"
+                continue
+            if V[s] == None:
+                continue
+            if V[t] == None:
+                V[t] = V[s] + d[e]
+                continue
             if V[s] + d[e] < V[t]:
                 V[t] = V[s] + d[e]
-    ans = V[N-1]
-    for _ in range(N):
-        for e in d:
-            s, t = e
-            if V[s] + d[e] < V[t]:
-                V[t] = V[s] + d[e]
+    # ans = V[N-1]
+    # for _ in range(N):
+    #     for e in d:
+    #         s, t = e
+    #         if V[s] + d[e] < V[t]:
+    #             V[t] = V[s] + d[e]
 
-    if V[N-1] < ans:
+    # if V[N-1] < ans:
+    #     print(-1)
+    #     return
+    # print( max(-ans,0))
+    if V[-1] == "a":
         print(-1)
-        return
-    print( max(-ans,0))
-    
+    else:
+        print(max(-W[-1],0))
 if __name__ == '__main__':
     main()
