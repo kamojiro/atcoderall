d2bbbff6b4e3a46ceec2b54f352f7be5f79d61e5
kamojiro <tamagoma002@yahoo.co.jp>
2019-11-18 22:02:25 +0900

    事前に考察してたもの

---
+++ regular/019/B/src/main.rs
@@ -0,0 +1,51 @@
+#![allow(non_snake_case)]
+fn main() {
+    let s = std::io::stdin();
+    let mut sc = Scanner { stdin: s.lock() };
+    let A = sc.chars();
+    let mut cnt:usize = 0;
+    let N = A.len();
+    for i in 0..(N/2){
+        if A[i] != A[N-1-i]{
+            cnt += 1;
+        }
+    }
+    let mut ans:usize = 25*N;
+
+    if cnt == 1{
+        ans -= 2;
+    }else if (cnt == 0) && (N%2 == 1){
+        ans -= 25;
+    }
+    println!("{}", ans);
+}
+
+pub struct Scanner<R> {
+    stdin: R,
+}
+
+impl<R: std::io::Read> Scanner<R> {
+    pub fn read<T: std::str::FromStr>(&mut self) -> T {
+        use std::io::Read;
+        let buf = self
+            .stdin
+            .by_ref()
+            .bytes()
+            .map(|b| b.unwrap())
+            .skip_while(|&b| b == b' ' || b == b'\n')
+            .take_while(|&b| b != b' ' && b != b'\n')
+            .collect::<Vec<_>>();
+        unsafe { std::str::from_utf8_unchecked(&buf) }
+        .parse()
+            .ok()
+            .expect("Parse error.")
+    }
+    pub fn vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.read()).collect()
+    }
+    pub fn chars(&mut self) -> Vec<char> {
+        self.read::<String>().chars().collect()
+    }
+}
+
+
