c8b83c81ac1cc1e4fddf7c2d9b516ac82d79398f
kamojiro <tamagoma002@yahoo.co.jp>
2019-12-16 00:19:27 +0900

    AGCの謎の問題。差分に注目。

---
+++ grand/017/B/src/main.rs
@@ -0,0 +1,50 @@
+#![allow(non_snake_case)]
+fn main() {
+    let s = std::io::stdin();
+    let mut sc = Scanner { stdin: s.lock() };
+    let N:usize = sc.read();
+    let A:i64 = sc.read();
+    let B:i64 = sc.read();
+    let C:i64 = sc.read();
+    let D:i64 = sc.read();
+    let mut ans = "NO";
+    let n = N as i64;
+    for m in 0..N{
+        let m = m as i64;
+        if (m*C - (n-1-m)*D <= B - A) && ( B-A <= m*D - (n-1-m)*C){
+            ans = "YES";
+            break
+        }
+    }
+    println!("{}", ans);
+}
+
+pub struct Scanner<R> {
+    stdin: R,
+}
+
+impl<R: std::io::Read> Scanner<R> {
+    pub fn read<T: std::str::FromStr>(&mut self) -> T {
+        use std::io::Read;
+        let buf = self
+            .stdin
+            .by_ref()
+            .bytes()
+            .map(|b| b.unwrap())
+            .skip_while(|&b| b == b' ' || b == b'\n')
+            .take_while(|&b| b != b' ' && b != b'\n')
+            .collect::<Vec<_>>();
+        unsafe { std::str::from_utf8_unchecked(&buf) }
+        .parse()
+            .ok()
+            .expect("Parse error.")
+    }
+    pub fn vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.read()).collect()
+    }
+    pub fn chars(&mut self) -> Vec<char> {
+        self.read::<String>().chars().collect()
+    }
+}
+
+
---
+++ regular/014/C/src/main.rs
@@ -0,0 +1,50 @@
+#![allow(non_snake_case)]
+fn main() {
+    let s = std::io::stdin();
+    let mut sc = Scanner { stdin: s.lock() };
+    let N:usize = sc.read();
+    let S:Vec<char> = sc.chars();
+    let mut r:usize = 0;
+    let mut g:usize = 0;
+    let mut b:usize = 0;
+    for i in 0..N{
+        if S[i] == 'R'{
+            r += 1;
+        }else if S[i] == 'G'{
+            g += 1;
+        }else if S[i] == 'B'{
+            b += 1;
+        }
+    }
+    println!("{}", r%2+g%2+b%2);
+}
+
+pub struct Scanner<R> {
+    stdin: R,
+}
+
+impl<R: std::io::Read> Scanner<R> {
+    pub fn read<T: std::str::FromStr>(&mut self) -> T {
+        use std::io::Read;
+        let buf = self
+            .stdin
+            .by_ref()
+            .bytes()
+            .map(|b| b.unwrap())
+            .skip_while(|&b| b == b' ' || b == b'\n')
+            .take_while(|&b| b != b' ' && b != b'\n')
+            .collect::<Vec<_>>();
+        unsafe { std::str::from_utf8_unchecked(&buf) }
+        .parse()
+            .ok()
+            .expect("Parse error.")
+    }
+    pub fn vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.read()).collect()
+    }
+    pub fn chars(&mut self) -> Vec<char> {
+        self.read::<String>().chars().collect()
+    }
+}
+
+
