3f786522826193adc60d3b2de201dbace61c2133
kamojiro <tamagoma002@yahoo.co.jp>
2020-03-08 11:51:05 +0900

    ABC158のE。具体的な数字で、確認すると良いかも。

--- beginner/158/E.py
+++ beginner/158/E.py
@@ -1,15 +1,23 @@
-#import sys
-#input = sys.stdin.readline
+# import sys
+# input = sys.stdin.readline
 def main():
     N, P = map( int ,input().split())
-    S = [ int(s) for s in list(input())][::-1]
+    S = [ int(s) for s in list(input().strip())][::-1]
+    if P == 2 or P == 5:
+        ans = 0
+        for i in range(N):
+            if S[i]%P == 0:
+                ans += N-i
+        print(ans)
+        return
     L = [0]*P
     L[0] = 1
     t = 0
-    for i in range(N):
-        t = (S[i]*pow(10,i,P)+t)%P
-        print(t)
+    s = 1
+    for z in S:
+        t = (z*s+t)%P
         L[t] += 1
+        s = s*10%P
     ans = 0
     for l in L:
         ans += l*(l-1)//2
